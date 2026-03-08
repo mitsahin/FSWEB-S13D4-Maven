f05f7b0e9bd7320a7e649cca1f2059748406a2a8
mitsahin <umit55goz@gmail.com>
2026-03-08 04:58:27 +0300

    Java Encapsulation player point weapon

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point first = new Point(6, 5);
+        Point second = new Point(3, 1);
+
+        System.out.println("distance(0,0)= " + first.distance());
+        System.out.println("distance(second)= " + first.distance(second));
+        System.out.println("distance(2,2)= " + first.distance(2, 2));
+
+        Point point = new Point(0, 0);
+        System.out.println("distance()= " + point.distance());
+
+        Player player = new Player("Ali", 120, Weapon.SWORD);
+        System.out.println(player.healthRemaining());
+
+        player.loseHealth(30);
+        System.out.println(player.healthRemaining());
+
+        player.loseHealth(80);
+        System.out.println(player.healthRemaining());
+
+        player.restoreHealth(50);
+        System.out.println(player.healthRemaining());
     }
 }
